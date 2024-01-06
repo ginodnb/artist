534e98c666a56620b62dc26181daa9c288ae8ec2
ginodnb <ginoarnone90@gmail.com>
2024-01-06 17:35:30 +0000

    fixed navbar

--- app.js
+++ app.js
@@ -8,17 +8,17 @@ const toggle = () => {
 
 };
 
-console.log("balls");
+// console.log("balls");
 
-let sum = 0;
-const numArray = [5, 17, 43.23, 16, -43, 18];
-function sum(arr) {
-    arr.forEach(function(number) {
-        total += 50;
-    });
-    return total;
-}
-console.log(sum(numArray))
+// let sum = 0;
+// const numArray = [5, 17, 43.23, 16, -43, 18];
+// function sum(arr) {
+//     arr.forEach(function(number) {
+//         total += 50;
+//     });
+//     return total;
+// }
+// console.log(sum(numArray))
 
 
 // numArray.forEach(sum =>  {
