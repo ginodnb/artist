0f985a81f52869d5f6cce03cc1b2ff75df585e8b
gino arnone <ginoarnone90@gmail.com>
2025-05-12 11:10:52 +0100

    updated link info on landing page

--- src/data/youFool.ts
+++ src/data/youFool.ts
@@ -1,17 +1,22 @@
 export const youFool = [
     {
-        title: 'You Fool',
+        title: 'Spotify',
         href: 'https://open.spotify.com/album/5NDr7TAPrZ4WCtLQwI5SEu?si=ZWe0zLjQS2KwjX3eJ9UuaQ',
         hrefLabel: 'Listen on Spotify',
     },
     {
-        title: 'Beat port',
+        title: 'Beatport',
         href: 'https://www.beatport.com/release/you-fool-more-of-you/4875974',
-        hrefLabel: 'Listen on Spotify',
+        hrefLabel: 'Buy on Beatport',
     },
     {
         title: 'Juno',
         href: 'https://www.junodownload.com/products/gino-you-fool-more-of-you/6936926-02/',
-        hrefLabel: 'Listen on Spotify',
+        hrefLabel: 'Buy on Juno',
+    },
+    {
+        title: 'Apple Music',
+        href: 'https://music.apple.com/gb/album/you-fool/1788055816?i=1788055817',
+        hrefLabel: 'Listen on Apple Music',
     }
 ]
