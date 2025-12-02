37b7b0980cc039e05f15b8ea75ae58ed4b852deb
gino arnone <ginoarnone90@gmail.com>
2025-12-02 15:53:35 +0000

    updated tour dates and ticket links

--- src/data/tourDates.ts
+++ src/data/tourDates.ts
@@ -2,20 +2,6 @@ export const tourDates = [
     {
         season: 'Q4 25 SZN',
         dates: [
-                        {
-                date: '15.11',
-                location: 'Amsterdam',
-                venue: 'Insomnia',
-                href: 'https://shop.weeztix.com/4b9da71a-8363-11f0-a9cb-7e126431635e/tickets?shop_code=q5edv69q&fbclid=PAZXh0bgNhZW0CMTEAc3J0YwZhcHBfaWQMMjU2MjgxMDQwNTU4AAGnpqh9c-L5xQfD_vD8RXG1Akra35usMWz8bUfZDmDTPsQGq2gh2DIs86HMtUk_aem_6d8bLbz2IaLn4pWJwNZSVg',
-                hrefLabel: 'Tickets',
-            },
-            {
-                date: '22.11',
-                location: 'Ghent, BE',
-                venue: 'Artcube',
-                href: 'https://shop.weeztix.com/8badc312-57cc-4380-92c1-5ee166b672d8/tickets?shop_code=y5vq5tv9',
-                hrefLabel: 'Tickets',
-            },
             {
                 date: '06.12',
                 location: 'Nottingham',
@@ -27,13 +13,20 @@ export const tourDates = [
                 date: '19.12',
                 location: 'Granada, ES',
                 venue: 'Industrial Copera',
-                href: '#',
-                hrefLabel: 'Tickets SOON',
+                href: 'https://industrialcopera.net/event/bass-diciembre-2025/',
+                hrefLabel: 'Tickets',
             },
             {
                 date: '27.12',
                 location: 'Basel',
                 venue: 'Info soon',
+                href: '#',
+                hrefLabel: 'Tickets SOON',
+            },
+            {
+                date: '31.12',
+                location: 'Bournemouth',
+                venue: 'The Vault',
                 href: '#',
                 hrefLabel: 'Tickets SOON',
             },
@@ -57,6 +50,13 @@ export const tourDates = [
                 venue: 'TBC',
                 href: '#',
                 hrefLabel: 'Tickets SOON',
+            },
+            {
+                date: '20.02',
+                location: 'Leeds',
+                venue: 'Mint Warehouse',
+                href: 'https://www.skiddle.com/whats-on/Leeds/Mint-Warehouse/Euphoria-3rd-Birthday/41354402/',
+                hrefLabel: 'Tickets',
             },
             
             
