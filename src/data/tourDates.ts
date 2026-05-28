be51249b09d0d5a96b07f90c5906e36cec94b1cb
gino arnone <ginoarnone90@gmail.com>
2026-05-28 12:45:10 +0100

    updated site

--- src/data/tourDates.ts
+++ src/data/tourDates.ts
@@ -9,27 +9,6 @@ export const tourDates = [
         season: 'NEW ZEALAND TOUR',
         dates: [
             {
-                date: '02.05',
-                location: 'Mallorca, Spain',
-                venue: 'Dnb allstars',
-                href: 'https://www.dnballstars.com/whats-on/dnb-allstars-mallorca',
-                hrefLabel: 'Tickets',
-            },
-            {
-                date: '22.05',
-                location: 'Glasgow',
-                venue: 'Pray to Good',
-                href: 'https://ra.co/events/2393807',
-                hrefLabel: 'Tickets',
-            },
-            {
-                date: '23.05',
-                location: 'Vienna, Austria',
-                venue: 'Arena',
-                href: 'https://www.collapse-recordings.com/?utm_source=ig&utm_medium=social&utm_content=link_in_bio&fbclid=PAZXh0bgNhZW0CMTEAc3J0YwZhcHBfaWQMMjU2MjgxMDQwNTU4AAGn7mMSoCCiMq3LuzZydRK2dU0X5QqsGE76eH__G5cJ-FYX5iSI8VPJz5ZphXo_aem_dZ_cYsGxs62GBXKJCN_GCA',
-                hrefLabel: 'Tickets',
-            },
-            {
                 date: '30.05',
                 location: 'Bristol, UK',
                 venue: 'lost horizon',
