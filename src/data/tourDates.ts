f406d0b22c195f712f5b51120a8f08ad6863b3ab
gino arnone <ginoarnone90@gmail.com>
2026-07-10 22:50:47 +0100

    updated ticket links

--- src/data/tourDates.ts
+++ src/data/tourDates.ts
@@ -8,33 +8,34 @@ export const tourDates = [
     {
         season: 'NEW ZEALAND TOUR',
         dates: [
+
             {
-                date: '30.05',
-                location: 'Bristol, UK',
-                venue: 'lost horizon',
-                href: 'https://www.skiddle.com/whats-on/Bristol/Lost-Horizon-HQ/Latte-Presents-Flow-State/42158243/',
-                hrefLabel: 'Tickets',
+                date: '01.09',
+                location: 'Let it roll, ',
+                venue: 'Lake Most',
+                href: '#',
+                hrefLabel: 'Tickets available',
             },
             {
-                date: '05.06',
-                location: 'Bologna, IT',
+                date: '07.09',
+                location: 'Peterborough',
+                venue: 'The Queens Head',
+                href: '#',
+                hrefLabel: 'Tickets available',
+            },
+            {
+                date: '19.09',
+                location: 'Worcester',
+                venue: 'Tramps',
+                href: '#',
+                hrefLabel: 'Tickets available',
+            },
+            {
+                date: '25.09',
+                location: 'Birmingham',
                 venue: 'TBA',
-                href: 'https://xceed.me/en/bologna/event/much-much-bass-presents-gino/231235/channel/kinder-srl?utm_source=ig&utm_medium=social&utm_content=link_in_bio&fbclid=PAZXh0bgNhZW0CMTEAc3J0YwZhcHBfaWQPOTM2NjE5NzQzMzkyNDU5AAGn_aGVKHE3SUAKaVDxGXNWYzlQtdBbSU-E0erakjU-RlLZD2C7PsH3khKEn4A_aem_5HxvVevKzbR4jaaE_DcPMg&utm_id=97760_v0_s00_e0_tv3',
-                hrefLabel: 'Tickets',
-            },
-            {
-                date: '14.06',
-                location: 'London',
-                venue: 'Dnb Allstars',
-                href: 'https://www.skiddle.com/whats-on/Ilford/Eutopia-Warehouse/DnB-Allstars---Summer-In-The-City---London/41966074/',
-                hrefLabel: 'Tickets',
-            },
-            {
-                date: '19.06',
-                location: 'Milton Keynes, UK',
-                venue: 'Unit 9',
-                href: 'https://www.skiddle.com/whats-on/Milton-Keynes/Unit-Nine/Turno--Friends---Milton-Keynes/42145958/',
-                hrefLabel: 'Tickets',
+                href: '#',
+                hrefLabel: 'Tickets soon',
             },
 
         ]
